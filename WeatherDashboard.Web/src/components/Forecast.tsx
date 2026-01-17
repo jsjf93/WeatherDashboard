db75d9354104a96986e299aa065dbe170f3a9271
jsjf93 <jsjf93@hotmail.co.uk>
2026-01-17 17:18:27 +0000

    Resolved minor spacing issues with the icons in the forecast

--- WeatherDashboard.Web/src/components/Forecast.tsx
+++ WeatherDashboard.Web/src/components/Forecast.tsx
@@ -7,25 +7,25 @@ export function Forecast() {
       <Card>
         <div className="flex flex-col gap-3 md:min-h-36">
           <h2 className="text-xs font-semibold uppercase">Forecast</h2>
-          <div className="flex items-center gap-4 whitespace-nowrap justify-between">
+          <div className="flex items-center whitespace-nowrap justify-between">
             <span className="w-20">Today</span>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
               <Sun size={24} className="shrink-0" />
-              <span className="font-semibold">17°C</span>
+              <span className="font-semibold w-12">17°C</span>
             </div>
           </div>
-          <div className="flex items-center gap-4 whitespace-nowrap justify-between">
+          <div className="flex items-center whitespace-nowrap justify-between">
             <span className="w-20">Tomorrow</span>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
               <Cloud size={24} className="shrink-0" />
-              <span className="font-semibold">19°C</span>
+              <span className="font-semibold w-12">19°C</span>
             </div>
           </div>
-          <div className="flex items-center gap-4 whitespace-nowrap justify-between">
+          <div className="flex items-center whitespace-nowrap justify-between">
             <span className="w-20">Wednesday</span>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
               <Sun size={24} className="shrink-0" />
-              <span className="font-semibold">20°C</span>
+              <span className="font-semibold w-12">20°C</span>
             </div>
           </div>
         </div>
